1bb1cd39cc0f9e232c0134b84c11febbeb90e11e
Samuel Tam <heiyiutam@gmail.com>
2023-01-26 19:49:18 +0800

    flake.nix: use `inputs` for extra attributes

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
   inputs.fiziko.url = "github:jemmybutton/fiziko";
   inputs.fiziko.flake = false;
 
-  outputs = { self, context-minimals, fiziko }: {
+  outputs = { self, context-minimals, ... }@inputs: {
     packages = context-minimals.lib.mkCompilation {
       src = self;
-      postUnpack = "install -Dt $sourceRoot ${fiziko}/fiziko.mp";
+      postUnpack = "install -Dt $sourceRoot ${inputs.fiziko}/fiziko.mp";
     };
     apps = context-minimals.lib.mkCompilationApps { };
   };
