71a217259369f130695f378ea5a30580908f6ca8
usertam <heiyiutam@gmail.com>
2023-03-12 18:41:48 +0800

    cinders year plan, version alpha

--- flake.nix
+++ flake.nix
@@ -3,8 +3,14 @@
   inputs.cover.url = "https://unsplash.com/photos/7K1_uSnNoy4/download";
   inputs.cover.flake = false;
 
-  outputs = { self, context-minimals, ... }@inputs: {
+  outputs = { self, context-minimals, ... }@inputs:
+  let
+    fonts = [ "source-sans-pro" "source-han-serif" ];
+    fcache = [ "sourcehanserif" ];
+    suffices = [ ".tex" ".png" ];
+  in {
     packages = context-minimals.lib.mkCompilation {
+      inherit fonts fcache suffices;
       src = self;
       nativeBuildInputs = [ "imagemagick" ];
       postUnpack = ''
@@ -12,6 +18,8 @@
           ${inputs.cover} "$sourceRoot/$(basename ${inputs.cover})"
       '';
     };
-    apps = context-minimals.lib.mkCompilationApps { };
+    apps = context-minimals.lib.mkCompilationApps {
+      inherit fonts fcache;
+    };
   };
 }
