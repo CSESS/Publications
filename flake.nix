c5d940ed674f626c05471a51f524030a20e70a60
usertam <heiyiutam@gmail.com>
2023-05-14 01:33:09 +0800

    cinders reimbursement of farewell dinner

--- flake.nix
+++ flake.nix
@@ -1,17 +1,12 @@
 {
   inputs.context-minimals.url = "github:usertam/context-minimals";
-  inputs.cover.url = "https://unsplash.com/photos/7K1_uSnNoy4/download";
-  inputs.cover.flake = false;
-
-  outputs = { self, context-minimals, ... }@inputs: {
-    packages = context-minimals.lib.mkCompilation {
-      src = self;
-      nativeBuildInputs = [ "imagemagick" ];
-      postUnpack = ''
-        convert -quality 100% -despeckle -rotate 180 \
-          ${inputs.cover} "$sourceRoot/cover.jpeg"
-      '';
-    };
-    apps = context-minimals.lib.mkCompilationApps { };
+  outputs = { self, context-minimals, ... }@inputs:
+  let
+    fonts = [ "source-sans-pro" "source-han-serif" ];
+    fcache = [ "sourcehanserif" ];
+    
+  in {
+    packages = context-minimals.lib.mkCompilation { inherit fonts fcache; src = self; };
+    apps = context-minimals.lib.mkCompilationApps { inherit fonts fcache; };
   };
 }
